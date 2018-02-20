91b863fe1092bf04b2adebe7f88ba765fb82a80f
David DUPUIS <david.c.dupuis@gmail.com>
2018-02-20 17:56:48 +0100

    spacing between function

--- RTIM.py
+++ RTIM.py
@@ -12,11 +12,13 @@ def inf_scores_graph(graph):
     ''' Compute the influence score of all the nodes in the graph '''
     pass
 
+
 def target(node):
     ''' Decide whether to target a node or not '''
     if graph[node]['ap'] > THETA_AP or graph[node]['inf'] < theta_inf:
         return False
     return True
+
 
 def update_ap(graph, node):
     '''
@@ -24,12 +26,14 @@ def update_ap(graph, node):
     '''
     pass
 
+
 def inf_threshold(inf_score_distribution):
     '''
         Compute influence threshold based on influence score distribution
         Selects threshold as top 10% of influencers
     '''
     pass
+
 
 if __name__ == "__main__":
     '''
