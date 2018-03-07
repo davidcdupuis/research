bd8e33880a8981684f231901e64b9dd1ba3424ea
David <david.c.dupuis@gmail.com>
2018-03-07 22:23:12 +0100

    improved user extractor.py

--- user_extractor.py
+++ user_extractor.py
@@ -11,19 +11,19 @@ if __name__ == "__main__":
 
     nodes = set()
     count = 0
-    with open('../TwitterData/twitter_rv.net', 'r') as f:
-        reader = csv.reader(f, delimiter='\t')
-        for line in reader:
-            count += 1
-            user = line[0]
-            follower = line[1]
-            if user not in nodes:
-                with open('../TwitterData/nodes.csv', 'a', newline='') as f:
-                    writer = csv.writer(f)
+    with open('../TwitterData/twitter_rv.net', 'r') as f1:
+        with open('../TwitterData/nodes.csv', 'a', newline='') as f2:
+            reader = csv.reader(f1, delimiter='\t')
+            writer = csv.writer(f2)
+            for line in reader:
+                count += 1
+                user = line[0]
+                follower = line[1]
+                if user not in nodes:
                     writer.writerow([user])
-                    writer.writerow([follower])
-            if len(nodes) == 1000:
-                nodes.pop()
-            nodes.add(user)
-            if count % 1000000 == 0:
-                print("Processed {} lines!".format(count))
+                writer.writerow([follower])
+                if len(nodes) == 1000:
+                    nodes.pop()
+                nodes.add(user)
+                if count % 1000000 == 0:
+                    print("Processed {} lines!".format(count))
