b481c65cb7304f0dfffe367bd23a9c6005578bbb
David DUPUIS <david.c.dupuis@gmail.com>
2018-03-08 18:45:52 +0100

    added sampling functions

--- user_extractor.py
+++ user_extractor.py
@@ -51,11 +51,64 @@ def extract_num_followers():
                     print("Processed {} lines!".format(count))
             writer.writerow([user, follower_count])
 
+def extract_sample_edges():
+    with open('../TwitterData/twitter_sample.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+    with open('../TwitterData/twitter_rv.net', 'r') as f1:
+        with open('../TwitterData/twitter_sample.csv', 'a', newline='') as f2:
+            reader = csv.reader(f1, delimiter='\t')
+            writer = csv.writer(f2, delimiter='\t')
+            line_count = 0
+            first_line = next(reader)
+            user = first_line[0]
+            user_count = 1
+            for line in reader:
+                line_count += 1
+                follower = line[1]
+                if line[0] != user:
+                    user = line[0]
+                    user_count = 1
+                    writer.writerow([user, follower])
+                else:
+                    if user_count <= 100:
+                        writer.writerow([user, follower])
+                        user_count += 1
+                if line_count >= 100000:
+                    break
+
+def extract_sample_users():
+    with open('../TwitterData/nodes_sample.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+
+    nodes = set()
+    count = 0
+    with open('../TwitterData/twitter_sample.csv', 'r') as f1:
+        with open('../TwitterData/nodes_sample.csv', 'a', newline='') as f2:
+            reader = csv.reader(f1, delimiter='\t')
+            writer = csv.writer(f2)
+            for line in reader:
+                count += 1
+                user = line[0]
+                follower = line[1]
+                if user not in nodes:
+                    writer.writerow([user])
+                    nodes.add(user)
+                if follower not in nodes:
+                    writer.writerow([follower])
+                if len(nodes) == 1000:
+                    nodes.pop()
+                if count % 50000 == 0:
+                    print("Processed {} lines!".format(count))
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Main")
     parser.add_argument("--users", default=False, action="store_true",
                         help="run extract_users")
     parser.add_argument("--followers", default=False, action="store_true",
+                        help="run extract_num_followers")
+    parser.add_argument("--sampleEdges", default=False, action="store_true",
+                        help="run extract_num_followers")
+    parser.add_argument("--sampleUsers", default=False, action="store_true",
                         help="run extract_num_followers")
     args = parser.parse_args()
 
@@ -63,3 +116,7 @@ if __name__ == "__main__":
         extract_users
     if args.followers:
         extract_num_followers()
+    if args.sampleEdges:
+        extract_sample_edges()
+    if args.sampleUsers:
+        extract_sample_users()
