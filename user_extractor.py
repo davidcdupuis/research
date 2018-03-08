c1df47cf116ccaff111ce2952d856a781f9b0c58
David DUPUIS <david.c.dupuis@gmail.com>
2018-03-08 17:27:10 +0100

    improved user_extractor + added function to compute numbe of followers of nodes in twitter_rv.net

--- user_extractor.py
+++ user_extractor.py
@@ -3,9 +3,9 @@
 '''
 
 import csv
+import argparse
 
-
-if __name__ == "__main__":
+def extract_users():
     with open('../TwitterData/nodes.csv', 'w', newline='') as f:
         writer = csv.writer(f)
 
@@ -27,3 +27,39 @@ if __name__ == "__main__":
                 nodes.add(user)
                 if count % 1000000 == 0:
                     print("Processed {} lines!".format(count))
+
+def extract_num_followers():
+    with open('../TwitterData/num_followers.csv', 'w', newline='') as f:
+        writer= csv.writer(f)
+    count = 0
+    with open('../TwitterData/twitter_rv.net', 'r') as f1:
+        with open('../TwitterData/num_followers.csv', 'a', newline='') as f2:
+            reader = csv.reader(f1, delimiter='\t')
+            writer = csv.writer(f2, delimiter='\t')
+            first_line = next(reader)
+            user = first_line[0]
+            follower_count = 1
+            for line in reader:
+                count += 1
+                if line[0] != user:
+                    writer.writerow([user, follower_count])
+                    user = line[0]
+                    follower_count = 1
+                else:
+                    follower_count += 1
+                if count % 10000000 == 0:
+                    print("Processed {} lines!".format(count))
+            writer.writerow([user, follower_count])
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Main")
+    parser.add_argument("--users", default=False, action="store_true",
+                        help="run extract_users")
+    parser.add_argument("--followers", default=False, action="store_true",
+                        help="run extract_num_followers")
+    args = parser.parse_args()
+
+    if args.users:
+        extract_users
+    if args.followers:
+        extract_num_followers()
