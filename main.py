2733445acc07453d376d278a4b5d8579034b0d74
David <david.c.dupuis@gmail.com>
2018-04-30 19:10:00 +0200

    added twitter to datasets

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import research_data
 import plot
 
 datasets = ['small_graph', 'hep', 'hept', 'phy', 'dblp', 'youtube', 'orkut',
-            'friendster', 'livejournal']
+            'friendster', 'livejournal', 'twitter']
 models = ['wc', '0.1', '0.01', '0.3', '0.5', '0.7', '0.8', '0.9', '1.0']
 algorithms = ['rtim', 'rand_repeat', 'rand_no_repeat', 'opt_size', 'test_import']
 
